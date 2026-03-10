2091bd9e4093814deeae996968faeea343902c57
sajid hussain <rehannetwork000@gmail.com>
2026-03-11 00:03:01 +0500

    update adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-2342049878330495" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2342049878330495"
